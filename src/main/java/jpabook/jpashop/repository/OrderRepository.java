bed92baf79de03bf83b022268572ddaeae29a3ba
rain46688 <rain46688@gmail.com>
2022-09-30 15:54:50 +0900

    부트 공부 18.0 - 쿼리 DSL 사용

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,18 +1,21 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.domain.Member;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
+@Slf4j
 @RequiredArgsConstructor
 public class OrderRepository {
 
@@ -29,38 +32,65 @@ public class OrderRepository {
     }
 
     // 오더 전체 조회 (검색 파라미터 넣어야되서 이건 동적 쿼리로 동작함 그래서 나중에 보기)
-    public List<Order> findAll(OrderSearch orderSearch){
-        // 이건 기본 쿼리
-//        return em.createQuery("select o from Order o join o.member m where o.status = :status and m.name like :name",Order.class)
-//                .setParameter("status", orderSearch.getOrderStatus())
-//                .setParameter("name", orderSearch.getMemberName())
-//                .getResultList();
+//    public List<Order> findAll(OrderSearch orderSearch){
+//        // 이건 기본 쿼리
+////        return em.createQuery("select o from Order o join o.member m where o.status = :status and m.name like :name",Order.class)
+////                .setParameter("status", orderSearch.getOrderStatus())
+////                .setParameter("name", orderSearch.getMemberName())
+////                .getResultList();
+//
+//        // 동적 쿼리
+//        // 이것도 나중에 쿼리 DSL로 변경할꺼라함
+//        // 이게 표준인데 쓰기 복잡하다고해서 실무에서는 잘 안씀
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+//        Root<Order> o = cq.from(Order.class);
+//        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+//        List<Predicate> criteria = new ArrayList<>();
+//        //주문 상태 검색
+//        if (orderSearch.getOrderStatus() != null) {
+//            Predicate status = cb.equal(o.get("status"),
+//                    orderSearch.getOrderStatus());
+//            criteria.add(status);
+//        }
+//        //회원 이름 검색
+//        if (StringUtils.hasText(orderSearch.getMemberName())) {
+//            Predicate name =
+//                    cb.like(m.<String>get("name"), "%" +
+//                            orderSearch.getMemberName() + "%");
+//            criteria.add(name);
+//        }
+//
+//        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+//        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
+//        return query.getResultList();
+//    }
 
-        // 동적 쿼리
-        // 이것도 나중에 쿼리 DSL로 변경할꺼라함
-        // 이게 표준인데 쓰기 복잡하다고해서 실무에서는 잘 안씀
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
-        Root<Order> o = cq.from(Order.class);
-        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
-        List<Predicate> criteria = new ArrayList<>();
-        //주문 상태 검색
-        if (orderSearch.getOrderStatus() != null) {
-            Predicate status = cb.equal(o.get("status"),
-                    orderSearch.getOrderStatus());
-            criteria.add(status);
+    public List<Order> findAll(OrderSearch orderSearch) {
+        // log.info("getOrderStatus : "+orderSearch.getOrderStatus() + "getMemberName : "+orderSearch.getMemberName());
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                        nameLike('%'+orderSearch.getMemberName()+'%'))
+                .limit(1000)
+                .fetch();
+    }
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
         }
-        //회원 이름 검색
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            Predicate name =
-                    cb.like(m.<String>get("name"), "%" +
-                            orderSearch.getMemberName() + "%");
-            criteria.add(name);
+        return QOrder.order.status.eq(statusCond);
+    }
+    private BooleanExpression nameLike(String nameCond) {
+        if (!StringUtils.hasText(nameCond)) {
+            return null;
         }
-
-        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
-        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
-        return query.getResultList();
+        return QMember.member.name.like(nameCond);
     }
 
 
